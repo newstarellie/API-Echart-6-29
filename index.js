1639e28626ffd525cf050721b0768e447e484f20
Ellie <zebra91528@gmail.com>
2022-06-29 22:51:14 +0800

    6/29

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const xhr = new XMLHttpRequest();
 
 xhr.open(
   'get',
-  'https://raw.githubusercontent.com/newstarellie/API-Echart-6-29/main/301000000A-002037-005.json',
+  'https://raw.githubusercontent.com/newstarellie/API-Echart-6-29/main/301000000A-001660-007.json',
   true
 );
 
@@ -10,6 +10,27 @@ xhr.send(null);
 
 xhr.onload = function () {
   let data = JSON.parse(xhr.responseText);
-  console.log(data.result.records[0]);
-  document.querySelector('.dataAll').textContent = data.result;
+  console.log(data.result.limit);
+
+  console.log(data.result.records[1].marriage_type);
+  document.querySelector('.dataAll').textContent =
+    data.result.records[5].marriage_type;
+
+  //for 抓到前五的婚姻類型
+  let marriage_type_diff_sex = 0;
+  let marriage_type_same_sex = 0;
+  for (let i = 1; i < 2000; i++) {
+    if (data.result.records[i].marriage_type == '不同性別') {
+      //console.log('不同性別');
+      marriage_type_diff_sex += 1;
+      //console.log(marriage_type_diff_sex);
+    } else {
+      //console.log('相同性別');
+      marriage_type_same_sex += 1;
+      //console.log(marriage_type_same_sex);
+    }
+  }
+  console.log(marriage_type_diff_sex);
+
+  console.log(marriage_type_same_sex);
 };
